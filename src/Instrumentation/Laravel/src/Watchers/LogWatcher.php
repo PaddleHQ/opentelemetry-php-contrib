ac611584a97a9d8dde5179a4243ad97488dd0979
Alec Sammon <alec.sammon@paddle.com>
2024-05-22 07:23:07 +0100

    Use configuration to determine log level

--- src/Instrumentation/Laravel/src/Watchers/LogWatcher.php
+++ src/Instrumentation/Laravel/src/Watchers/LogWatcher.php
@@ -6,13 +6,21 @@ namespace OpenTelemetry\Contrib\Instrumentation\Laravel\Watchers;
 
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Log\Events\MessageLogged;
-use Illuminate\Log\LogManager;
 use OpenTelemetry\API\Trace\Span;
 use OpenTelemetry\Context\Context;
 
 class LogWatcher extends Watcher
 {
-    private LogManager $logger;
+    private const DEBUG = 1;
+    private const INFO = 2;
+    private const NOTICE = 3;
+    private const WARNING = 4;
+    private const ERROR = 5;
+    private const CRITICAL = 6;
+    private const ALERT = 7;
+    private const EMERGENCY = 8;
+
+    private int $minLogLevel;
 
     /** @psalm-suppress UndefinedInterfaceMethod */
     public function register(Application $app): void
@@ -20,8 +28,11 @@ class LogWatcher extends Watcher
         /** @phan-suppress-next-line PhanTypeArraySuspicious */
         $app['events']->listen(MessageLogged::class, [$this, 'recordLog']);
 
-        /** @phan-suppress-next-line PhanTypeArraySuspicious */
-        $this->logger = $app['log'];
+        $this->minLogLevel = self::INFO;
+        $levelStr = strtoupper(env("OTEL_PHP_LOG_LEVEL", "INFO"));
+        if (defined(self::class.'::'.$levelStr)) {
+            $this->minLogLevel = constant(self::class . '::' . $levelStr);
+        }
     }
 
     /**
@@ -29,10 +40,10 @@ class LogWatcher extends Watcher
      */
     public function recordLog(MessageLogged $log): void
     {
-        $underlyingLogger = $this->logger->getLogger();
-
-        /** @phan-suppress-next-line PhanUndeclaredMethod */
-        if (method_exists($underlyingLogger, 'isHandling') && !$underlyingLogger->isHandling($log->level)) {
+        if (
+            defined(self::class.'::'.strtoupper($log->level)) &&
+            constant(self::class . '::' . strtoupper($log->level)) < $this->minLogLevel
+        ) {
             return;
         }
 
